849cffccb0bce09c85ce210a47dd00f7f41c286a
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-06-19 09:13:23 +0530

    Time: 5 ms (42.82%), Space: 47.1 MB (13.95%) - LeetHub

---
+++ 2871-double-a-number-represented-as-a-linked-list/2871-double-a-number-represented-as-a-linked-list.java
@@ -0,0 +1,34 @@
+class Solution {
+    public ListNode doubleIt(ListNode head) {
+        if (head == null) return null;
+        head = reverse(head);
+        ListNode temp = head;
+        int carry = 0;
+
+        while (temp!= null) {
+            int val = temp.val * 2 + carry;
+            carry = val / 10;
+            temp.val = val % 10;
+            if (temp.next==null && carry > 0) {
+                temp.next = new ListNode(carry);
+                carry = 0;
+                break;
+            }
+            temp = temp.next;
+            
+        }
+        return reverse(head);
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr!= null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
