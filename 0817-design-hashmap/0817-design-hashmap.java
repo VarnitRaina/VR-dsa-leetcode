a587f5b55bffd0676da084852420d5af0da1986a
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-07-11 22:34:14 +0530

    Time: 18 ms (68.96%), Space: 55.2 MB (14.46%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.java
+++ 0817-design-hashmap/0817-design-hashmap.java
@@ -2,19 +2,18 @@ class MyHashMap {
     int map[];
     public MyHashMap() {
         map=new int[1000001];
-        for(int i=0;i<map.length;i++)map[i]=-1;
     }
     
     public void put(int key, int value) {
-        map[key]=value;
+        map[key]=value+1;
     }
     
     public int get(int key) {
-        return map[key];
+        return map[key]-1;
     }
     
     public void remove(int key) {
-        map[key]=-1;
+        map[key]=0;
     }
 }
 
