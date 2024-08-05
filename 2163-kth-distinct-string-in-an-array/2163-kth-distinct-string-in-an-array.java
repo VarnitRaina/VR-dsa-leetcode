e98414150aa26ce24447a755e1832c990690d992
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-08-05 13:03:40 +0530

    Time: 5 ms (99.47%), Space: 44.1 MB (51.23%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.java
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.java
@@ -1,26 +1,27 @@
 class Solution {
     public String kthDistinct(String[] arr, int k) {
-        List<String> al=Arrays.asList(arr);
-        ArrayList<String> l1=new ArrayList<>(al);
-        ArrayList<String> l2=new ArrayList<>();
-        for(int i=0;i<l1.size();i++){
-            boolean isDistinct=true;
-            for(int j=0;j<l1.size();j++){
-                if(i!=j && l1.get(i).equals(l1.get(j))){
-                    isDistinct=false;
-                    break;
-                }
+        HashSet<String> distinct=new HashSet<>();
+        HashSet<String> duplicate=new HashSet<>();
+        for(String str:arr){
+            if(duplicate.contains(str)){
+                continue;
             }
-            if(isDistinct){
-                l2.add(l1.get(i));
+            if(distinct.contains(str)){
+                distinct.remove(str);
+                duplicate.add(str);
+            }
+            else{
+                distinct.add(str);
             }
         }
-        
-        if((k-1)<l2.size()){
-            return l2.get(k-1);
+        for(String str:arr){
+            if(!duplicate.contains(str)){
+                k--;
+            }
+            if(k==0){
+                return str;
+            }
         }
-        else{
-            return "";
-        }
+        return "";
     }
 }
