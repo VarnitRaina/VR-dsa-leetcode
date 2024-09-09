96f066bcba0d887caff91b85ec55eac16c42246d
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-09-09 23:06:26 +0530

    Time: 0 ms (100%), Space: 43.9 MB (90.34%) - LeetHub

--- 2416-evaluate-boolean-binary-tree/2416-evaluate-boolean-binary-tree.java
+++ 2416-evaluate-boolean-binary-tree/2416-evaluate-boolean-binary-tree.java
@@ -15,9 +15,8 @@
  */
 class Solution {
     public boolean evaluateTree(TreeNode root) {
-        if(root.left==null && root.right==null){
+        if(root.left==null && root.right==null)
             return (root.val==1)?true:false;
-        }
         boolean left=false;
         boolean right=false;
         if(root.left!=null){
