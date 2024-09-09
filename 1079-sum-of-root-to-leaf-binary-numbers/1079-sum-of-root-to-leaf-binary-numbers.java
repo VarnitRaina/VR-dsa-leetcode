a2fc4bf36a24fe3ec38d72c30b022eaa112720e6
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-09-10 01:34:46 +0530

    Time: 0 ms (100%), Space: 42.1 MB (38.73%) - LeetHub

--- 1079-sum-of-root-to-leaf-binary-numbers/1079-sum-of-root-to-leaf-binary-numbers.java
+++ 1079-sum-of-root-to-leaf-binary-numbers/1079-sum-of-root-to-leaf-binary-numbers.java
@@ -17,9 +17,9 @@ class Solution {
     public int sumRootToLeaf(TreeNode root) {
         return sumRoot(root,0);
     }
-    public int sumRoot(TreeNode root,int val) {
+    public int sumRoot(TreeNode root,int val){
         if(root==null)return 0;
-        val=(val*2+root.val);
-        return root.left==root.right?val:sumRoot(root.left,val)+sumRoot(root.right,val);
+        val=2*val+root.val;
+        return (root.left==root.right)?val:sumRoot(root.left,val)+sumRoot(root.right,val);
     }
 }
