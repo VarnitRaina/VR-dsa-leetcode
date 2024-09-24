daa67b1a5d0fea108d976780cac13f973b2b5cd3
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-09-24 19:40:15 +0530

    Time: 1 ms (96.92%), Space: 44.1 MB (78.48%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,21 +15,21 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Map<Integer ,Integer> map=new HashMap<>();
+        Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<inorder.length;i++){
             map.put(inorder[i],i);
         }
-        return splitTree(preorder,map,0,0,inorder.length-1);
+        return tree(preorder,map,0,0,inorder.length-1);
     }
-    public TreeNode splitTree(int[] preorder,Map<Integer,Integer> map,int rootIndex,int left,int right){
+    public TreeNode tree(int[] preorder,Map<Integer,Integer> map,int rootIndex,int left,int right){
         if(left>right)return null;
         TreeNode root=new TreeNode(preorder[rootIndex]);
         int mid=map.get(preorder[rootIndex]);
         if(mid>left){
-            root.left=splitTree(preorder,map,rootIndex+1,left,mid-1);
+            root.left=tree(preorder,map,rootIndex+1,left,mid-1);
         }
         if(mid<right){
-            root.right=splitTree(preorder,map,rootIndex+mid-left+1,mid+1,right);
+            root.right=tree(preorder,map,rootIndex+mid-left+1,mid+1,right);
         }
         return root;
     }
