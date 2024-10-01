46c82fa8a4d293da241e47df58cec56a404c51db
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-10-01 19:08:40 +0530

    Time: 2 ms (5.47%), Space: 42.6 MB (93.82%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -15,24 +15,23 @@
  */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> l1=new ArrayList<>();
-        int height=height(root);
-        for(int i=height;i>0;i--){
+       List<List<Integer>> l1=new ArrayList<>();
+       int height=height(root);
+       for(int i=height;i>0;i--){
             List<Integer> l2=new ArrayList<>();
             traverse(root,l2,i);
             l1.add(l2);
         }
         return l1;
     }
-    public void traverse(TreeNode root,List<Integer> list1,int count){
+    public void traverse(TreeNode root,List<Integer> list,int count){
         if(root==null)return;
-        if(count==1){
-            list1.add(root.val);
-        }
+        if(count==1)list.add(root.val);
         else if(count>1){
-            traverse(root.left,list1,count-1);
-            traverse(root.right,list1,count-1);
+            traverse(root.left,list,count-1);
+            traverse(root.right,list,count-1);
         }
+
     }
     public int height(TreeNode root){
         if(root==null)return 0;
