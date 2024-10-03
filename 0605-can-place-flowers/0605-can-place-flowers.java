af69b070983f88cfabcb3915b503d2779f7ee994
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-10-03 23:12:17 +0530

    Time: 1 ms (96.83%), Space: 45.5 MB (63.41%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -5,12 +5,13 @@ class Solution {
             if(flowerbed[i]==0){
                 int prev=(i==0 || flowerbed[i-1]==0)?0:1;
                 int next=(i==flowerbed.length-1 || flowerbed[i+1]==0)?0:1;
-
                 if(prev==0 && next==0){
-                flowerbed[i]=1;     
-                count++;
-            }
+                    flowerbed[i]=1;
+                    count++;
+                    i++;
+                }
             }
         }
-        return count>=n;
-}}
+        return count>=n;   
+    }
+}
