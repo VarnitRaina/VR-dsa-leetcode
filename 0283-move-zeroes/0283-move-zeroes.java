afb5b85348a081893d9cdef08a0488eccf24968d
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-10-26 19:48:37 +0530

    Time: 1 ms (99.77%), Space: 46.2 MB (11.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-            int count=0;
-            int n=nums.length;
-            for(int i=0;i<n;i++){
-               if (nums[i]!=0){
-                 nums[count++]=nums[i];
+        int count=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[count++]=nums[i];
             }
         }
-        for(int j=count;j<n;j++){
-            nums[j]=0;
+        for(int i=count;i<n;i++){
+            nums[i]=0;
         }
     }
 }
