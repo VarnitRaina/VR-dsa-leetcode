3fc77f130cb98f8e185692554795131f85512110
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-11-07 00:30:03 +0530

    Time: 1 ms (86.71%), Space: 42.5 MB (9.16%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
+    public List<List<Integer>> generate(int numRows){
         List<List<Integer>> result=new ArrayList<>();
         for(int i=0;i<numRows;i++){
             List<Integer> row=new ArrayList<>();
@@ -8,7 +8,7 @@ class Solution {
                     row.add(1);
                 }
                 else{
-                   row.add(result.get(i-1).get(j-1)+result.get(i-1).get(j));
+                    row.add(result.get(i-1).get(j-1)+result.get(i-1).get(j));
                 }
             }
             result.add(row);
