788ceb587a660d53316c5cff5de414f2e676bcb6
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-11-30 21:37:19 +0530

    Time: 0 ms (100%), Space: 44.8 MB (9.03%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.java
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -1,9 +1,7 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
-        for(int arr[]:image){
-            int start=0;
-            int end=arr.length-1;
-            reverse(arr,start,end);
+        for(int [] arr:image){
+            reverse(arr,0,arr.length-1);
             for(int i=0;i<arr.length;i++){
                 if(arr[i]==1){
                     arr[i]=0;
@@ -15,7 +13,7 @@ class Solution {
         }
         return image;
     }
-    public void reverse(int[] arr, int start,int end){
+    public void reverse(int[] arr,int start,int end){
         while(start<end){
             int temp=arr[start];
             arr[start++]=arr[end];
