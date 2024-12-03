870a27a752252766096682720a8663037cfae8e4
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-12-03 19:53:00 +0530

    Time: 0 ms (100%), Space: 44.5 MB (89.82%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int prev=0;
+        int curr=0;
+        int n=nums.length;
+        while(curr<n){
+            if(nums[curr]%2==0){
+                swap(nums,prev,curr);
+                curr++;
+                prev++;
+            }
+            else{
+                curr++;
+            }
+        }
+        return nums;
+    }
+    public void swap(int arr[],int start,int end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+    }
+}
