f577cb5df0ebaf26d9a407c018aa3d483cb503b6
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2024-12-09 14:42:21 +0530

    Time: 164 ms (5.09%), Space: 45.2 MB (24.49%) - LeetHub

---
+++ 0979-di-string-match/0979-di-string-match.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int i=0;
+        int d=s.length();
+        int res[]=new int[s.length()+1];
+        for(int j=0;j<s.length();j++){
+            if(s.charAt(j)=='I'){
+                res[j]=i++;
+            }
+            else{
+                res[j]=d--;
+            }
+        }
+        res[s.length()]=i;
+        return res;
+    }
+}
