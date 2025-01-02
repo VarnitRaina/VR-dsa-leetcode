74bdb2397d3517c32d288ca252c978709ae24983
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2025-01-02 13:48:48 +0530

    Time: 1 ms (100%), Space: 43.5 MB (40.12%) - LeetHub

--- 3165-find-indices-with-index-and-value-difference-i/3165-find-indices-with-index-and-value-difference-i.java
+++ 3165-find-indices-with-index-and-value-difference-i/3165-find-indices-with-index-and-value-difference-i.java
@@ -5,14 +5,10 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 if(Math.abs(i-j)>=indexDifference && Math.abs(nums[i]-nums[j])>=valueDifference){
-                    ans[0]=i;
-                    ans[1]=j;
-                    return ans;
+                    return new int[]{i,j};
                 }
             }
         }
-        ans[0]=-1;
-        ans[1]=-1;
-        return ans;
+        return new int[]{-1,-1};
     }
 }
