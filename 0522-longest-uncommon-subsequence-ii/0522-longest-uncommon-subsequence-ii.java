58b3de77d8b37a5e37b916da19d44d3d9a430219
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2025-02-18 13:39:12 +0530

    Time: 1 ms (98.04%), Space: 41.3 MB (46.67%) - LeetHub

--- 0522-longest-uncommon-subsequence-ii/0522-longest-uncommon-subsequence-ii.java
+++ 0522-longest-uncommon-subsequence-ii/0522-longest-uncommon-subsequence-ii.java
@@ -1,15 +1,16 @@
 class Solution {
     public int findLUSlength(String[] strs) {
         Arrays.sort(strs,(a,b)->b.length()-a.length());
-        for(int i=0;i<strs.length;i++){
-            boolean isSubseq=false;
-            for(int j=0;j<strs.length;j++){
+        int n=strs.length;
+        for(int i=0;i<n;i++){
+            boolean subseq=false;
+            for(int j=0;j<n;j++){
                 if(i!=j && isSubsequence(strs[i],strs[j])){
-                    isSubseq=true;
+                    subseq=true;
                     break;
                 }
             }
-            if(!isSubseq){
+            if(!subseq){
                 return strs[i].length();
             }
         }
@@ -17,12 +18,13 @@ class Solution {
     }
     public boolean isSubsequence(String s1,String s2){
         int i=0,j=0;
-        while(i<s1.length() && j<s2.length()){
+        int n=s1.length(),m=s2.length();
+        while(i<n && j<m){
             if(s1.charAt(i)==s2.charAt(j)){
                 i++;
             }
             j++;
         }
-        return i==s1.length();
+        return i==n;
     }
 }
