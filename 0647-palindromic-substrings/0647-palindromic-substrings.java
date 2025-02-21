47692a731e142b373df6dcbb5c8201c33e424305
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2025-02-21 12:42:53 +0530

    Time: 5 ms (96.74%), Space: 41.6 MB (73.27%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -3,20 +3,19 @@ class Solution {
         int n=s.length();
         int count=0;
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isPalindrome(s,i,j)){
-                    count++;
-                }
-            }
+            count+=expandAroundCenter(s,i,i);
+            count+=expandAroundCenter(s,i,i+1);
         }
         return count;
     }
-    public boolean isPalindrome(String s,int start,int end){
-        while(start<end){
-            if(s.charAt(start++)!=s.charAt(end--)){
-                return false;
-            }
+    public int expandAroundCenter(String s,int start,int end){
+        int n=s.length();
+        int count=0;
+        while(start>=0 && end<n && s.charAt(start)==s.charAt(end)){
+            start--;
+            end++;
+            count++;
         }
-        return true;
+        return count;
     }
 }
