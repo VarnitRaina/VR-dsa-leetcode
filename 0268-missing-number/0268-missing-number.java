1a5ce6fb7812e508bd81445a330bcf912fbc0e69
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2025-04-07 11:32:19 +0530

    Time: 0 ms (100%), Space: 45.1 MB (84.51%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,10 +1,8 @@
 class Solution {
-    public int missingNumber(int[] nums) {
-        int xor=nums.length;
-        for(int i=0;i<nums.length;i++){
-            xor=xor^i;
-            xor=xor^nums[i];
+    public int missingNumber(int[] nums){
+                    int m=nums.length;
+        for (int i=0;i<nums.length;i++){
+            m+=i-nums[i];
         }
-        return xor;
-    }
-}
+        return m;
+}}
