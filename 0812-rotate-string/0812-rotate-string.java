aab5f63903531d7265a6e7c47938a41b8d8ac2ce
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2025-04-16 15:16:17 +0530

    Time: 0 ms (100%), Space: 41.2 MB (96.13%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,14 +1,6 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-      if(s.length()!=goal.length())return false;
-      for(int i=0;i<s.length();i++){
-        if(s.equals(goal)){
-            return true;
-        }
-        else{
-            s=s.substring(1).concat(s.substring(0,1));
-        }
-      }
-      return false;
+        if(s.length()!=goal.length())return false;
+        return (s+s).contains(goal);
     }
 }
