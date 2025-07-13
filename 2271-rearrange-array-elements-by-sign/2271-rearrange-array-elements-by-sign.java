c5b5389f355ccafab6ed8da08dc7a119a9735423
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2025-07-14 04:01:05 +0530

    Time: 10 ms (15.15%), Space: 78.3 MB (33.67%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -3,24 +3,19 @@ class Solution {
         List<Integer> l1=new ArrayList<>();
         List<Integer> l2=new ArrayList<>();
         int n=nums.length;
-        int[] result=new int[n];
-        for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                l1.add(nums[i]);
+        for(int num:nums){
+            if(num>=0){
+                l1.add(num);
             }
             else{
-                l2.add(nums[i]);
+                l2.add(num);
             }
         }
-        int pind=0,nind=0;
+        int pos=0,neg=0;
         for(int i=0;i<n;i++){
-            if(i%2==0){
-                result[i]=l1.get(pind++);
-            }
-            else{
-                result[i]=l2.get(nind++);
-            }
+            if(i%2==0)nums[i]=l1.get(pos++);
+            else nums[i]=l2.get(neg++);
         }
-        return result;
+        return nums;
     }
 }
