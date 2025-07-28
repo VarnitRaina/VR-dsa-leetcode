6dc5389d089dc186207af642eab9c5c6d5db57e7
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2025-07-29 01:37:49 +0530

    Time: 0 ms (100%), Space: 45.7 MB (66.76%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,32 +2,22 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix[0].length;
         int n=matrix.length;
+        int cols[]=new int[m];
+        int rows[]=new int[n];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    zeroRow(matrix,i,m);
-                    zeroCol(matrix,j,n);
+                    rows[i]=1;
+                    cols[j]=1;
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==-99){
+                if(rows[i]==1 || cols[j]==1){
                     matrix[i][j]=0;
                 }
             }
-        }
-    }
-    public void zeroRow(int nums[][],int a,int b){
-        for(int k=0;k<b;k++){
-            if(nums[a][k]!=0)
-                nums[a][k]=-99;
-        }
-    }
-    public void zeroCol(int nums[][],int a,int b){
-        for(int k=0;k<b;k++){
-            if(nums[k][a]!=0)
-                nums[k][a]=-99;
         }
     }
 }
