e37a20e4fa9fa863b93a9c2a85ec663aa3f62378
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2025-08-03 20:57:51 +0530

    Time: 1 ms (99.54%), Space: 56.9 MB (70.26%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum=0,max=Integer.MIN_VALUE,n=nums.length;
+        int sum=0,max=Integer.MIN_VALUE;
         for(int num:nums){
             if(sum<0)sum=0;
             sum+=num;
