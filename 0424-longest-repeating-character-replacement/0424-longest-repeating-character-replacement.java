3a61d37a4769c5074ef115beaa1618ed725d503f
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2025-08-16 17:56:00 +0530

    Time: 10 ms (67.24%), Space: 44.9 MB (59.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,19 +1,17 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int n=s.length(),res=0,max=0,l=0;
+        int n=s.length();
         int arr[]=new int[26];
+        int res=0,max=0;
+        int l=0;
         for(int r=0;r<n;r++){
             arr[s.charAt(r)-'A']++;
-            if(arr[s.charAt(r)-'A']>max){
-                max=arr[s.charAt(r)-'A'];
-            }
-            if((r-l+1-max)>k){
+            max=Math.max(max,arr[s.charAt(r)-'A']);
+            if(r-l+1-max>k){
                 arr[s.charAt(l)-'A']--;
                 l++;
             }
-            if(r-l+1>res){
-                res=r-l+1;
-            }
+            res=Math.max(res,r-l+1);
         }
         return res;
     }
