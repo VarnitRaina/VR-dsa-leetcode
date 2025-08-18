d0615a12afeb0cf96cd25bb432159801c74d6f35
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2025-08-18 17:22:52 +0530

    Time: 11 ms (78.26%), Space: 55.2 MB (48.01%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -2,7 +2,7 @@ class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         return helper(nums,k)-helper(nums,k-1);
     }
-    public int helper(int[]nums,int k){
+    public int helper(int[] nums,int k){
         int l=0,n=nums.length,count=0,odd=0;
         for(int r=0;r<n;r++){
             odd+=nums[r]%2;
@@ -10,7 +10,7 @@ class Solution {
                 odd-=nums[l]%2;
                 l++;
             }
-           count+=r-l+1;
+            count+=r-l+1;
         }
         return count;
     }
