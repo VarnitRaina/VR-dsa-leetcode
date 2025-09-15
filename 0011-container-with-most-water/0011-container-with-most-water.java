77d35305d12fef21fcaede7204f4e6a5358671f7
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2025-09-15 19:11:35 +0530

    Time: 4 ms (91.7%), Space: 58.2 MB (32.5%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,10 +3,14 @@ class Solution {
         int n=height.length;
         int l=0,r=n-1,max=0;
         while(l<r){
-            int area=Math.min(height[l],height[r])*(r-l);
-            if(area>max)max=area;
-            if(height[l]<height[r])l++;
-            else r--;
+            int vol=Math.min(height[l],height[r])*(r-l);
+            if(vol>max)max=vol;
+            if(height[l]<height[r]){
+                l++;
+            }
+            else{
+                r--;
+            }
         }
         return max;
     }
