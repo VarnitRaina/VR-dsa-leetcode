75531265323fc2039d080f2bd344a0c69661e26c
Varnit Raina <127969144+VarnitRaina@users.noreply.github.com>
2025-09-17 00:50:39 +0530

    Time: 7 ms (98.51%), Space: 46.6 MB (57.02%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         List<int[]> list=new ArrayList<>();
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
         int n=intervals.length;
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
         int currentInterval[]=intervals[0];
         list.add(currentInterval);
         for(int i=1;i<n;i++){
